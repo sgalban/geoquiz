d67a096cee405983c20dc2653c7a24c6192d58c1
sgalban <sgalban@seas.upenn.edu>
2018-04-14 21:03:59 -0400

    Created welcome page

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-
 var express = require('express');
+var path = require("path");
 var app = express();
 
 app.set('port', (process.env.PORT || 8888));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+  response.sendFile(path.join(__dirname, "/", "main.html"));
 });
 
 app.listen(app.get('port'), function() {
