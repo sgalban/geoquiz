4f7f9dbd15499ceaacf53dbf322a696e050b70d1
sgalban <sgalban@seas.upenn.edu>
2018-04-16 14:10:33 -0400

    Introduced quiz page

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', function(request, response) {
   response.sendFile(path.join(__dirname, "/", "main.html"));
 });
+app.get('/quiz', function(request, response) {
+  response.sendFile(path.join(__dirname, "/", "quiz.html"));
+});
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
