5ce805084b26041df78f19890736c81cf513e0e9
sgalban <sgalban@seas.upenn.edu>
2018-04-21 09:56:44 -0400

    Added placeholder question text (for formatting purposes)

--- index.js
+++ index.js
@@ -5,13 +5,19 @@ var app = express();
 app.set('port', (process.env.PORT || 8888));
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(request, response) {
-  response.sendFile(path.join(__dirname, "/", "main.html"));
-});
-app.get('/quiz', function(request, response) {
-  response.sendFile(path.join(__dirname, "/", "quiz.html"));
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, "/", "main.html"));
 });
 
+app.get('/quiz', function(req, res) {
+  res.sendFile(path.join(__dirname, "/", "quiz.html"));
+});
+
+app.get('/generate-question', function(req, res) {
+    //TODO
+    res.send("Placeholder Question");
+});
+    
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 });
