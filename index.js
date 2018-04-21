d9166b332da9917d47cb84c5e0d551d5075529eb
sgalban <sgalban@seas.upenn.edu>
2018-04-21 13:15:46 -0400

    GET (placeholder) question from server

--- index.js
+++ index.js
@@ -5,19 +5,35 @@ var app = express();
 app.set('port', (process.env.PORT || 8888));
 app.use(express.static(__dirname + '/public'));
 
+/* Get the main page html */
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, "/", "main.html"));
 });
 
+/* Get the quiz page html */
 app.get('/quiz', function(req, res) {
   res.sendFile(path.join(__dirname, "/", "quiz.html"));
 });
 
+/* Create a question, as well as 4 sample answers. Do not send
+   the correct answer in the response. The order of the answers
+   should not indicate which one is correct */
 app.get('/generate-question', function(req, res) {
-    //TODO
-    res.send("Placeholder Question");
+    //TODO: This is a placeholder. Remember to query the DB
+    res.send({
+        question: "Placeholder Question",
+        answer1: "Answer 1",
+        answer2: "Answer 2",
+        answer3: "Answer 3",
+        answer4: "Answer 4"
+    });
 });
-    
+
+/* Verify that the selected answer is correct */
+app.get('/verify-answer', function(req, res) {
+    //TODO
+});
+
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 });
--- public/scripts/quiz-start.js
+++ public/scripts/quiz-start.js
@@ -1,1 +1,11 @@
 var app = angular.module("Quiz", []);
+
+app.controller("questionController", function($scope, $http) {
+    var req = $http.get("generate-question");
+    req.success(function(data) {
+        $scope.questionData = data;
+    });
+    req.error(function(data) {
+        console.log("Error: Cannot GET question");
+    });
+});
