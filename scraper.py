a6491f2b05d4d5cc4327be08f63c8972bce186ef
akshay <akcm96>
2018-04-25 00:33:16 -0400

    sql scripts for db

--- scraper.py
+++ scraper.py
@@ -5,6 +5,14 @@ import json
 
 def parse_cities():
 	count = 0
+	data = json.load(open('db_files/all.json'))
+	code_dict = {}
+
+	for i in range(94):
+		code = data[i]["Code"]
+		name = data[i]["Government"]["Country name"]["conventional short form"]["text"]
+		code_dict[name] = code
+
 	link = 'https://en.wikipedia.org/wiki/List_of_largest_cities'
 	response = requests.get(link)
 	soup = BeautifulSoup(response.text, "html.parser")
@@ -14,22 +22,49 @@ def parse_cities():
 		if city:
 			attributes = [el.text.encode('utf-8') for el in rows.find_all('td')]
 			country = attributes[0].split('\xc2\xa0')[1]
-			print(country)
-
 			population = attributes[2].split('[')[0]
-			city_dictionary[city.text.encode('utf-8').strip()] = {'country':country.strip(), 'population':population.strip()}
+			population = population.replace(',', '').strip()
+			if country == 'Korea, South':
+				country = 'South Korea'
+			#if country == 'Korea, North':
+			#	country = 'North Korea'
 			count += 1
+			city_text = city.text.encode('utf-8').strip()
+			if city_text == "Xi'an":
+				continue
+			unavail_countries = ['DR Congo', 'Myanmar', 'Ivory Coast', 'Afghanistan', 'Korea, North', 'United Arab Emirates', 'Nicaragua', 'Cambodia', 'Burkina Faso', 'Cuba', 'Yemen', 'Congo Republic', 'Mozambique', 'Zambia', '', 'Zimbabwe', 'Uganda', 'Czech Republic', 'Senegal', 
+			'Georgia', 'Armenia']
+			if country in unavail_countries:
+				continue
+			if population == ' 1029556':
+				population = population[1:]
+			city_dictionary[city_text] = {'country':code_dict[country.strip()], 'population':population}
 		if count == 241:
 			break
 
 	with open('city_result.json', 'w') as fp:
 		json.dump(city_dictionary, fp)
 
+	with open('city_populate.sql', 'w') as fp:
+		for city in city_dictionary.keys():
+			population = city_dictionary[city]['population']
+			country = city_dictionary[city]['country'].encode('utf-8')
+			fp.write("INSERT INTO Cities (name, countryCode, population) VALUES (\'" + city + "\', " + "\'" + country + "\', " + population + ");\n")
+
 def parse_mountains():
 	link = 'https://en.wikipedia.org/wiki/List_of_highest_mountains_on_Earth'
 	response = requests.get(link)
 	soup = BeautifulSoup(response.text, "html.parser")
 	mountain_dictionary = {}
+
+	data = json.load(open('db_files/all.json'))
+	code_dict = {}
+
+	for i in range(94):
+		code = data[i]["Code"]
+		name = data[i]["Government"]["Country name"]["conventional short form"]["text"]
+		code_dict[name] = code
+
 	for rows in soup.find_all('tr'):
 		mountain = rows.find_all('td')
 		if len(mountain) == 11:
@@ -60,18 +95,29 @@ def parse_mountains():
 				country = 'China'
 			elif 'Jongsong' in mountain[1].text:
 				country = 'China'
+
+			unavail_countries = ['Nepal', 'Bhutan', 'Tajikistan']
+			if country in unavail_countries:
+				continue
 		
 			peak = mountain[1].text.encode('utf-8', 'ignore').strip().split('/')[0].split('[')[0].strip()
 			elevation = mountain[3].text.encode('utf-8', 'ignore').strip()
-			mountain_dictionary[peak] = {'elevation':elevation, 'country':country}
+			elevation = elevation.replace(',', '').strip()
+			mountain_dictionary[peak] = {'elevation':elevation, 'country':code_dict[country.strip()]}
 
 	with open('mountain_result.json', 'w') as fp:
 		json.dump(mountain_dictionary, fp)
 
+	with open('mountain_populate.sql', 'w') as fp:
+		for mountain in mountain_dictionary.keys():
+			elevation = mountain_dictionary[mountain]['elevation']
+			country = mountain_dictionary[mountain]['country'].encode('utf-8')
+			fp.write("INSERT INTO Mountains (name, countryCode, elevation) VALUES (\'" + mountain + "\', " + "\'" + country + "\', " + elevation + ");\n")
+
 
 def main():
 	parse_cities()
-	#parse_mountains()
+	parse_mountains()
 
 if __name__ == '__main__':
 	main()
